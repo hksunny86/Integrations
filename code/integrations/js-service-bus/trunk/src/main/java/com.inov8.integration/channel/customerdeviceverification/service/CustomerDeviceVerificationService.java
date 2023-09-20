67fa79afba42022e1b6a996ae06d19e66f7c8305
Hamza Rajpoot <hamza.rajpoot@appinsnap.com>
2023-09-20 12:57:25 +0500

    ssl certificate handle

--- code/integrations/js-service-bus/trunk/src/main/java/com.inov8.integration/channel/customerdeviceverification/service/CustomerDeviceVerificationService.java
+++ code/integrations/js-service-bus/trunk/src/main/java/com.inov8.integration/channel/customerdeviceverification/service/CustomerDeviceVerificationService.java
@@ -11,6 +11,11 @@ import com.inov8.integration.channel.refferalCustomer.service.RefferalCustomerSe
 import com.inov8.integration.config.PropertyReader;
 import com.inov8.integration.i8sb.vo.I8SBSwitchControllerRequestVO;
 import com.inov8.integration.util.JSONUtil;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +23,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Service;
@@ -25,6 +31,12 @@ import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
 
 @Service
 public class CustomerDeviceVerificationService {
@@ -64,9 +76,9 @@ public class CustomerDeviceVerificationService {
             logger.info("Request Send To Get Customer Device Verification List: " + requesJson);
             T24ApiMockService mock = new T24ApiMockService();
 
-            String response = mock.transactionValidation();
+            String response = mock.customerDeviceDetails();
             customerDeviceVerificationResponse = (CustomerDeviceVerificationResponse) JSONUtil.jsonToObject(response, CustomerDeviceVerificationResponse.class);
-
+            Objects.requireNonNull(customerDeviceVerificationResponse).setResponseCode("00");
             //            logger.info("Response Code for Ibft Title Fetch Request : " + ibftTitleFetchResponse.getISOMessage().getResponseCode_039());
         } else {
             logger.info("Refferal Customer " + getDeviceVerificationList);
@@ -84,11 +96,11 @@ public class CustomerDeviceVerificationService {
                 }
             }
             try {
-            logger.info("Request Send to Get CustomerDevice Verification "+httpEntity);
-            ResponseEntity<String> res = restTemplate.postForEntity(uri.build().toUri(), httpEntity, String.class);
-            logger.info("Get Customer Device Verification List Response Received from Server 1: " + res.getBody());
-            customerDeviceVerificationResponse = (CustomerDeviceVerificationResponse) JSONUtil.jsonToObject(res.getBody(), CustomerDeviceVerificationResponse.class);
-            logger.info("Get Customer Device Verification List Response Received from Server : " + res.getStatusCode());
+                logger.info("Request Send to Get CustomerDevice Verification " + httpEntity);
+                ResponseEntity<String> res = getRestTemplate().postForEntity(uri.build().toUri(), httpEntity, String.class);
+                logger.info("Get Customer Device Verification List Response Received from Server 1: " + res.getBody());
+                customerDeviceVerificationResponse = (CustomerDeviceVerificationResponse) JSONUtil.jsonToObject(res.getBody(), CustomerDeviceVerificationResponse.class);
+                logger.info("Get Customer Device Verification List Response Received from Server : " + res.getStatusCode());
             } catch (RestClientException e) {
                 if (e instanceof HttpStatusCodeException) {
                     String response = ((HttpStatusCodeException) e).getStatusCode().toString();
@@ -99,16 +111,14 @@ public class CustomerDeviceVerificationService {
                         String resp = ((HttpStatusCodeException) e).getResponseBodyAsString();
                         customerDeviceVerificationResponse = (CustomerDeviceVerificationResponse) JSONUtil.jsonToObject(resp, CustomerDeviceVerificationResponse.class);
 
-                    }
-                    else if (response.equals("400")) {
+                    } else if (response.equals("400")) {
                         String resp = ((HttpStatusCodeException) e).getResponseBodyAsString();
                         customerDeviceVerificationResponse = (CustomerDeviceVerificationResponse) JSONUtil.jsonToObject(resp, CustomerDeviceVerificationResponse.class);
 
                     } else if (response.equals("403")) {
                         logger.info("Get Customer Device Verification List Response Received from Server RestClient Error : " + response);
 
-                    }
-                    else {
+                    } else {
                         logger.info("Get Customer Device Verification List Response Received from Server RestClient Error : " + response);
 
                     }
@@ -154,7 +164,7 @@ public class CustomerDeviceVerificationService {
             }
             try {
 
-                ResponseEntity<String> res = restTemplate.postForEntity(uri.build().toUri(), httpEntity, String.class);
+                ResponseEntity<String> res = getRestTemplate().postForEntity(uri.build().toUri(), httpEntity, String.class);
                 customerDeviceUpdateResponse = (CustomerDeviceUpdateResponse) JSONUtil.jsonToObject(res.getBody(), CustomerDeviceUpdateResponse.class);
                 logger.info("Customer Device Verification Response Received from Server : " + res.getBody());
             } catch (RestClientException e) {
@@ -230,4 +240,92 @@ public class CustomerDeviceVerificationService {
         return customerDeviceUpdateResponse;
     }
 
+    public RestTemplate getRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        TrustStrategy acceptingTrustStrategy = new TrustStrategy() {
+            @Override
+            public boolean isTrusted(java.security.cert.X509Certificate[] x509Certificates, String s) {
+                return true;
+            }
+
+        };
+
+        SSLContext sslContext = null;
+        try {
+            sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy)
+                    .build();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
+        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+
+        restTemplate.setRequestFactory(requestFactory);
+        return restTemplate;
+    }
+
+//    public static void main(String[] args) {
+//        RestTemplate restTemplate = new RestTemplate();
+//        CustomerDeviceVerificationRequest request = new CustomerDeviceVerificationRequest();
+//        CustomerDeviceVerificationResponse customerDeviceVerificationResponse = new CustomerDeviceVerificationResponse();
+//        request.setMobileNo("03028563561");
+//        try {
+//            {
+//                logger.info("Refferal Customer " + "https://zindigi-andriod.jsbl.com:8443/api/Zindigi/CustomerAuthenticationStatus");
+//                UriComponentsBuilder uri = UriComponentsBuilder.fromUriString("https://zindigi-andriod.jsbl.com:8443/api/Zindigi/CustomerAuthenticationStatus");
+//                HttpHeaders headers = new HttpHeaders();
+//                headers.setContentType(MediaType.APPLICATION_JSON);
+//                headers.add("Access_token", "$81*4A&d3L@!");
+//                String requesJson = JSONUtil.getJSON(request);
+//                logger.info("Request Send To Get Customer Device Verification List:" + requesJson);
+//                HttpEntity<?> httpEntity = new HttpEntity<>(requesJson, headers);
+//
+//                for (HttpMessageConverter converter : restTemplate.getMessageConverters()) {
+//                    if (converter instanceof StringHttpMessageConverter) {
+//                        ((StringHttpMessageConverter) converter).setWriteAcceptCharset(false);
+//                    }
+//                }
+//                try {
+//                    logger.info("Request Send to Get CustomerDevice Verification " + httpEntity);
+//                    ResponseEntity<String> res = getRestTemplate().postForEntity(uri.build().toUri(), httpEntity, String.class);
+//                    logger.info("Get Customer Device Verification List Response Received from Server 1: " + res.getBody());
+//                    customerDeviceVerificationResponse = (CustomerDeviceVerificationResponse) JSONUtil.jsonToObject(res.getBody(), CustomerDeviceVerificationResponse.class);
+//                    logger.info("Get Customer Device Verification List Response Received from Server : " + res.getStatusCode());
+//                } catch (RestClientException e) {
+//                    if (e instanceof HttpStatusCodeException) {
+//                        String response = ((HttpStatusCodeException) e).getStatusCode().toString();
+//                        if (response.equals("204")) {
+//                            String result = ((HttpStatusCodeException) e).getResponseBodyAsString();
+//                            customerDeviceVerificationResponse = (CustomerDeviceVerificationResponse) JSONUtil.jsonToObject(result, CustomerDeviceVerificationResponse.class);
+//                        } else if (response.equals("405")) {
+//                            String resp = ((HttpStatusCodeException) e).getResponseBodyAsString();
+//                            customerDeviceVerificationResponse = (CustomerDeviceVerificationResponse) JSONUtil.jsonToObject(resp, CustomerDeviceVerificationResponse.class);
+//
+//                        } else if (response.equals("400")) {
+//                            String resp = ((HttpStatusCodeException) e).getResponseBodyAsString();
+//                            customerDeviceVerificationResponse = (CustomerDeviceVerificationResponse) JSONUtil.jsonToObject(resp, CustomerDeviceVerificationResponse.class);
+//
+//                        } else if (response.equals("403")) {
+//                            logger.info("Get Customer Device Verification List Response Received from Server RestClient Error : " + response);
+//
+//                        } else {
+//                            logger.info("Get Customer Device Verification List Response Received from Server RestClient Error : " + response);
+//
+//                        }
+//
+//                    }
+//                }
+//
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }

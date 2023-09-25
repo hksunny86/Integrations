c3d6a7c5adc40e2a5db03918d1f2341a0f9f2db2
Hamza Rajpoot <hamza.rajpoot@appinsnap.com>
2023-09-25 11:40:07 +0500

    L2 account request/response

---
+++ code/integrations/js-blb-integration/trunk/src/main/java/com/inov8/integration/l2Account/pdu/response/L2AccountResponse.java
@@ -0,0 +1,84 @@
+package com.inov8.integration.l2Account.pdu.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.inov8.integration.webservice.l2Account.L2Account;
+
+import java.io.Serializable;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "Rrn",
+        "ResponseCode",
+        "ResponseDescription",
+        "ResponseDateTime",
+        "L2Accounts",
+        "HashData"
+})
+public class L2AccountResponse implements Serializable {
+
+    private final static long serialVersionUID = 1L;
+
+    @JsonProperty("Rrn")
+    private String rrn;
+    @JsonProperty("ResponseCode")
+    private String responseCode;
+    @JsonProperty("ResponseDescription")
+    private String responseDescription;
+    @JsonProperty("ResponseDateTime")
+    private String responseDateTime;
+    @JsonProperty("L2Accounts")
+    private List<L2Account> l2AccountList;
+    @JsonProperty("HashData")
+    private String hashData;
+
+    public String getRrn() {
+        return rrn;
+    }
+
+    public void setRrn(String rrn) {
+        this.rrn = rrn;
+    }
+
+    public String getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(String responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getResponseDescription() {
+        return responseDescription;
+    }
+
+    public void setResponseDescription(String responseDescription) {
+        this.responseDescription = responseDescription;
+    }
+
+    public String getResponseDateTime() {
+        return responseDateTime;
+    }
+
+    public void setResponseDateTime(String responseDateTime) {
+        this.responseDateTime = responseDateTime;
+    }
+
+    public List<L2Account> getL2AccountList() {
+        return l2AccountList;
+    }
+
+    public void setL2AccountList(List<L2Account> l2AccountList) {
+        this.l2AccountList = l2AccountList;
+    }
+
+    public String getHashData() {
+        return hashData;
+    }
+
+    public void setHashData(String hashData) {
+        this.hashData = hashData;
+    }
+}

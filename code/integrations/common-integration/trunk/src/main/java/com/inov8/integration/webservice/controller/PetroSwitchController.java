c12265244e2e82e97fdc9842b6b9610c7f4e7e7c
Muhammad Aqeel <muhammad.aqeel@appinsnap.com>
2024-04-29 11:41:14 +0500

    Petro Pocket

---
+++ code/integrations/common-integration/trunk/src/main/java/com/inov8/integration/webservice/controller/PetroSwitchController.java
@@ -0,0 +1,12 @@
+package com.inov8.integration.webservice.controller;
+
+import com.inov8.integration.webservice.vo.WebServiceVO;
+
+public interface PetroSwitchController {
+
+    WebServiceVO petroInquiry(WebServiceVO webServiceVO);
+    WebServiceVO petroPayment(WebServiceVO webServiceVO);
+    WebServiceVO petroWalletToWalletInquiry(WebServiceVO webServiceVO);
+    WebServiceVO petroWalletToWalletPayment(WebServiceVO webServiceVO);
+
+}

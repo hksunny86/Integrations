d0a255a6a0d182fa10f238cecbef5a437ce864f1
ahsan <ahsan.raza@appinsnap.com>
2024-05-23 16:06:22 +0500

    ultra Account Controller

---
+++ code/integrations/common-integration/trunk/src/main/java/com/inov8/integration/webservice/controller/UltraAccountSwitchController.java
@@ -0,0 +1,11 @@
+package com.inov8.integration.webservice.controller;
+
+import com.inov8.integration.webservice.vo.WebServiceVO;
+
+public interface UltraAccountSwitchController {
+
+
+    WebServiceVO l2AccountStatus(WebServiceVO webServiceVO);
+    WebServiceVO motherNames(WebServiceVO webServiceVO);
+
+}

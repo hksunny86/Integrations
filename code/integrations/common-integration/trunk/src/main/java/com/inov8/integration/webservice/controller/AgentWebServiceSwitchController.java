414a7fdd5d293a1ea8b5b34b8812d47177ee752d
Hamza Rajpoot <hamza.rajpoot@appinsnap.com>
2024-11-07 17:27:50 +0500

    agent mate portal apis

---
+++ code/integrations/common-integration/trunk/src/main/java/com/inov8/integration/webservice/controller/AgentWebServiceSwitchController.java
@@ -0,0 +1,16 @@
+package com.inov8.integration.webservice.controller;
+
+import com.inov8.integration.webservice.vo.WebServiceVO;
+
+public interface AgentWebServiceSwitchController {
+
+    WebServiceVO agentAccountLogin(WebServiceVO webServiceVO);
+
+    WebServiceVO agentLoginPinGeneration(WebServiceVO webServiceVO);
+
+    WebServiceVO agentLoginPinReset(WebServiceVO webServiceVO);
+
+    WebServiceVO agentMpinGeneration(WebServiceVO webServiceVO);
+
+    WebServiceVO agentMpinReset(WebServiceVO webServiceVO);
+}

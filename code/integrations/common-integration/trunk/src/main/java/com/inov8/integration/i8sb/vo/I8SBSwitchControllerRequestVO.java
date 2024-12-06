8572d7dbfda19fcc13d9e19378dd31b60f3b3057
Hamza Rajpoot <hamza.rajpoot@appinsnap.com>
2024-12-06 10:57:20 +0500

    request vo params added for euronet

--- code/integrations/common-integration/trunk/src/main/java/com/inov8/integration/i8sb/vo/I8SBSwitchControllerRequestVO.java
+++ code/integrations/common-integration/trunk/src/main/java/com/inov8/integration/i8sb/vo/I8SBSwitchControllerRequestVO.java
@@ -2,6 +2,8 @@ package com.inov8.integration.i8sb.vo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.inov8.integration.webservice.crpVO.Payload;
+import com.inov8.integration.webservice.euronetVO.InAccount;
+import com.inov8.integration.webservice.euronetVO.InAuditDS;
 import com.inov8.integration.webservice.islamicSavingVO.Body;
 import com.inov8.integration.webservice.optasiaVO.AdditionalInfo;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
@@ -535,6 +537,339 @@ public class I8SBSwitchControllerRequestVO implements Serializable {
     private String pdfEndDate;
     private String generatedBy;
     private String billNumber;
+    private String inFunction;
+    private String participantID;
+    private String inCzID;
+    private String inCIFkey;
+    private String incardNumber;
+    private String inMember;
+    private String inBIN;
+    private String inCustSchemeCode;
+    private String inReqCardType;
+    private String inCardType;
+    private String inIBAN;
+    private String inLanguage;
+    private String inSearchName;
+    private String inTitle;
+    private String inFName;
+    private String inMName;
+    private String inLName;
+    private String inSufx;
+    private String inEmbName;
+    private String inAddress1;
+    private String inAddress2;
+    private String inAddress3;
+    private String inAddress4;
+    private String inCity;
+    private String instate;
+    private String inCountry;
+    private String inZIPCode;
+    private String inAddtype;
+    private String inHPhone;
+    private String inDOB;
+    private String inSex;
+    private String inEmpFlg;
+    private String inCorPer;
+    private String issuanceFee;
+    private String issuanceFED;
+    private InAuditDS inAuditDS;
+    private InAccount inAccount;
+
+    public InAuditDS getInAuditDS() {
+        return inAuditDS;
+    }
+
+    public void setInAuditDS(InAuditDS inAuditDS) {
+        this.inAuditDS = inAuditDS;
+    }
+
+    public InAccount getInAccount() {
+        return inAccount;
+    }
+
+    public void setInAccount(InAccount inAccount) {
+        this.inAccount = inAccount;
+    }
+
+    public String getInFunction() {
+        return inFunction;
+    }
+
+    public void setInFunction(String inFunction) {
+        this.inFunction = inFunction;
+    }
+
+    public String getParticipantID() {
+        return participantID;
+    }
+
+    public void setParticipantID(String participantID) {
+        this.participantID = participantID;
+    }
+
+    public String getInCzID() {
+        return inCzID;
+    }
+
+    public void setInCzID(String inCzID) {
+        this.inCzID = inCzID;
+    }
+
+    public String getInCIFkey() {
+        return inCIFkey;
+    }
+
+    public void setInCIFkey(String inCIFkey) {
+        this.inCIFkey = inCIFkey;
+    }
+
+    public String getIncardNumber() {
+        return incardNumber;
+    }
+
+    public void setIncardNumber(String incardNumber) {
+        this.incardNumber = incardNumber;
+    }
+
+    public String getInMember() {
+        return inMember;
+    }
+
+    public void setInMember(String inMember) {
+        this.inMember = inMember;
+    }
+
+    public String getInBIN() {
+        return inBIN;
+    }
+
+    public void setInBIN(String inBIN) {
+        this.inBIN = inBIN;
+    }
+
+    public String getInCustSchemeCode() {
+        return inCustSchemeCode;
+    }
+
+    public void setInCustSchemeCode(String inCustSchemeCode) {
+        this.inCustSchemeCode = inCustSchemeCode;
+    }
+
+    public String getInReqCardType() {
+        return inReqCardType;
+    }
+
+    public void setInReqCardType(String inReqCardType) {
+        this.inReqCardType = inReqCardType;
+    }
+
+    public String getInCardType() {
+        return inCardType;
+    }
+
+    public void setInCardType(String inCardType) {
+        this.inCardType = inCardType;
+    }
+
+    public String getInIBAN() {
+        return inIBAN;
+    }
+
+    public void setInIBAN(String inIBAN) {
+        this.inIBAN = inIBAN;
+    }
+
+    public String getInLanguage() {
+        return inLanguage;
+    }
+
+    public void setInLanguage(String inLanguage) {
+        this.inLanguage = inLanguage;
+    }
+
+    public String getInSearchName() {
+        return inSearchName;
+    }
+
+    public void setInSearchName(String inSearchName) {
+        this.inSearchName = inSearchName;
+    }
+
+    public String getInTitle() {
+        return inTitle;
+    }
+
+    public void setInTitle(String inTitle) {
+        this.inTitle = inTitle;
+    }
+
+    public String getInFName() {
+        return inFName;
+    }
+
+    public void setInFName(String inFName) {
+        this.inFName = inFName;
+    }
+
+    public String getInMName() {
+        return inMName;
+    }
+
+    public void setInMName(String inMName) {
+        this.inMName = inMName;
+    }
+
+    public String getInLName() {
+        return inLName;
+    }
+
+    public void setInLName(String inLName) {
+        this.inLName = inLName;
+    }
+
+    public String getInSufx() {
+        return inSufx;
+    }
+
+    public void setInSufx(String inSufx) {
+        this.inSufx = inSufx;
+    }
+
+    public String getInEmbName() {
+        return inEmbName;
+    }
+
+    public void setInEmbName(String inEmbName) {
+        this.inEmbName = inEmbName;
+    }
+
+    public String getInAddress1() {
+        return inAddress1;
+    }
+
+    public void setInAddress1(String inAddress1) {
+        this.inAddress1 = inAddress1;
+    }
+
+    public String getInAddress2() {
+        return inAddress2;
+    }
+
+    public void setInAddress2(String inAddress2) {
+        this.inAddress2 = inAddress2;
+    }
+
+    public String getInAddress3() {
+        return inAddress3;
+    }
+
+    public void setInAddress3(String inAddress3) {
+        this.inAddress3 = inAddress3;
+    }
+
+    public String getInAddress4() {
+        return inAddress4;
+    }
+
+    public void setInAddress4(String inAddress4) {
+        this.inAddress4 = inAddress4;
+    }
+
+    public String getInCity() {
+        return inCity;
+    }
+
+    public void setInCity(String inCity) {
+        this.inCity = inCity;
+    }
+
+    public String getInstate() {
+        return instate;
+    }
+
+    public void setInstate(String instate) {
+        this.instate = instate;
+    }
+
+    public String getInCountry() {
+        return inCountry;
+    }
+
+    public void setInCountry(String inCountry) {
+        this.inCountry = inCountry;
+    }
+
+    public String getInZIPCode() {
+        return inZIPCode;
+    }
+
+    public void setInZIPCode(String inZIPCode) {
+        this.inZIPCode = inZIPCode;
+    }
+
+    public String getInAddtype() {
+        return inAddtype;
+    }
+
+    public void setInAddtype(String inAddtype) {
+        this.inAddtype = inAddtype;
+    }
+
+    public String getInHPhone() {
+        return inHPhone;
+    }
+
+    public void setInHPhone(String inHPhone) {
+        this.inHPhone = inHPhone;
+    }
+
+    public String getInDOB() {
+        return inDOB;
+    }
+
+    public void setInDOB(String inDOB) {
+        this.inDOB = inDOB;
+    }
+
+    public String getInSex() {
+        return inSex;
+    }
+
+    public void setInSex(String inSex) {
+        this.inSex = inSex;
+    }
+
+    public String getInEmpFlg() {
+        return inEmpFlg;
+    }
+
+    public void setInEmpFlg(String inEmpFlg) {
+        this.inEmpFlg = inEmpFlg;
+    }
+
+    public String getInCorPer() {
+        return inCorPer;
+    }
+
+    public void setInCorPer(String inCorPer) {
+        this.inCorPer = inCorPer;
+    }
+
+    public String getIssuanceFee() {
+        return issuanceFee;
+    }
+
+    public void setIssuanceFee(String issuanceFee) {
+        this.issuanceFee = issuanceFee;
+    }
+
+    public String getIssuanceFED() {
+        return issuanceFED;
+    }
+
+    public void setIssuanceFED(String issuanceFED) {
+        this.issuanceFED = issuanceFED;
+    }
 
     public String getBillNumber() {
         return billNumber;
